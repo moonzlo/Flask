0bdbbd00221e875e1a4861ebad0013439617e886
moonz <mihail.moonz@gmail.com>
2018-03-10 14:52:49 +0300

    тестовый html

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')  # '@' Означет что это вызов метода Дикоратора, он оборачивает результат функции НАД которой стоит.
 def index():  # Определяем первую страницу
-    return '<h1>Hello Syka</h1>'  # Возвращаем содержимое страницы.
+    name = 'Мишаня'
+    return render_template('index.html', n=name)  # Возвращаем содержимое страницы и переменной.
