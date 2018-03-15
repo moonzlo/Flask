936e808a15cbec7f0f2fc4272843a001b71569a7
moonz <mihail.moonz@gmail.com>
2018-03-15 19:03:12 +0300

    Импортировал библиотеки Flask security

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_security import SQLAlchemyUserDatastore, Security, current_user
+from flask import  redirect, url_for, request
+from flask_admin import Admin, AdminIndexView
+
 
 
 
@@ -17,7 +21,51 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
+
 from models import *
-admin = Admin(app)  # Подключили админку
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+
+# Доступ к админке
+
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+# Класс создания слага (человекопободнойссылки)
+class BaseModeView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModeView, self).on_model_change(form, model, is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+
+
+class PostAdminView(AdminMixin, BaseModeView):
+    form_columns = ['title', 'body', 'tags']
+
+
+class TagAdminView(AdminMixin, BaseModeView):
+    form_columns = ['name', 'posts']
+
+
+
+admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='Home'))  # Подключили админку
+admin.add_view(PostAdminView(Post, db.session))
+admin.add_view(TagAdminView(Tag, db.session))
+
+
+
+# Создаем user
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
